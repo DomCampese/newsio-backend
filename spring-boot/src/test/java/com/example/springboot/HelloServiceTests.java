c134783665a7eec7becb7c51b8adb67b710b6cf1
Dominic Campese <62118391+DomCampese@users.noreply.github.com>
2023-01-30 00:38:08 -0500

    Added example unit tests for the example service

---
+++ spring-boot/src/test/java/com/example/springboot/HelloServiceTests.java
@@ -0,0 +1,29 @@
+package com.example.springboot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.newsio.services.HelloService;
+
+@SpringBootTest(classes=HelloService.class)
+class HelloServiceTests {
+
+	@Autowired
+	private HelloService helloService;
+
+	@Test
+	void sayHelloReturnsHello() {
+		String result = helloService.sayHello();
+		assertEquals("Hello world!", result);
+	}
+
+	@Test
+	void thankUserForGreetingThanksUserForAppropriateGreeting() {
+		String greeting = "ayo";
+		String result = helloService.thankUserForGreeting(greeting);
+		assertEquals("Thanks for saying " + greeting, result);
+	}
+}
