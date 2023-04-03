fdcca7265f66a0df9fee085e4241bcfdc4fd5449
Dominic Campese <62118391+DomCampese@users.noreply.github.com>
2023-04-03 01:09:43 -0400

    Hacky, but got some params hard coded to make news come in order and in english, we should probably let user configure this in the future

--- spring-boot/src/main/java/com/newsio/services/MediaStackService.java
+++ spring-boot/src/main/java/com/newsio/services/MediaStackService.java
@@ -1,8 +1,10 @@
 package com.newsio.services;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,12 @@ public class MediaStackService {
     HttpClient httpClient;
 
     public String sendGet(String searchText) throws Exception {
-        String url = "http://api.mediastack.com/v1/news?access_key=88c7ef9c74b205e86bc9af36f0ba94cd&keywords=";
+        String sortParam = "&sort=" + URLEncoder.encode("published_desc", StandardCharsets.UTF_8);
+        String languageParam = "&languages=en";
+        String url = "http://api.mediastack.com/v1/news?access_key=88c7ef9c74b205e86bc9af36f0ba94cd" + sortParam + languageParam + "&language=en&keywords=";
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create(url.concat(searchText)))
+                .uri(URI.create(url.concat(URLEncoder.encode(searchText, StandardCharsets.UTF_8))))
                 .setHeader("User-Agent", "Java 11 HttpClient Bot")
                 .build();
 
