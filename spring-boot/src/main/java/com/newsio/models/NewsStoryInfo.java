64650d4ec1ea94188258789e0ad159d077e5215a
Jason <jason.kozarsky@gmail.com>
2023-04-04 16:13:40 -0400

    Added simple tag for JSON deserialization

--- spring-boot/src/main/java/com/newsio/models/NewsStoryInfo.java
+++ spring-boot/src/main/java/com/newsio/models/NewsStoryInfo.java
@@ -1,10 +1,13 @@
 package com.newsio.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 @Data
 @NoArgsConstructor
